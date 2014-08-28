9e00d50b4763c655d2d4176b8b29fe2cad2233b5
Gregory Jefferis <jefferis@gmail.com>
2014-08-28 08:14:01 +0100

    #include RcppEigen and use some classes

--- src/rcpp_hello_world.cpp
+++ src/rcpp_hello_world.cpp
@@ -1,7 +1,12 @@
-
 #include <Rcpp.h>
+#include <RcppEigen.h>
 using namespace Rcpp;
 
+using Eigen::Map;                 // 'maps' rather than copies 
+using Eigen::MatrixXd;                  // variable size matrix, double precision
+using Eigen::VectorXd;                  // variable size vector, double precision
+
+// [[Rcpp::depends(RcppEigen)]]
 // [[Rcpp::export]]
 List rcpp_hello_world() {
 
