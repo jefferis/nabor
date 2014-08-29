812dd485fbe8eea67b8a9cbb005e3c9e4a21acf9
Gregory Jefferis <jefferis@gmail.com>
2014-08-29 10:28:25 +0100

    more tests confirming equivalence of WKNNF/nn2
    * absolutely no differences
    * coming to conclusion that the problem is somehow related to R data
      structures coming in or out in knn

--- tests/testthat/test-WKNN.R
+++ tests/testthat/test-WKNN.R
@@ -26,3 +26,50 @@ test_that("Queries using WKNNF objects", {
   expect_equal(w1$queryWKNNF(w2$.CppObject, 1, 0), nn2(p1, p2, k=1), tolerance=1e-6)
 })
 
+knn_nn2_form<-function(d, q, k, eps=0){
+  knnresult <- knn(t(d), t(q), k=k, eps=eps)
+  # transpose and rename
+  structure(lapply(knnresult, t), .Names=c("nn.idx", "nn.dists"))
+}
+
+test_that("equivalence of WKNNF and knn, nn2 queries", {
+  p1=kcpoints[[1]]
+  w1=WKNNF(p1)
+  p2=kcpoints[[2]]
+  w2=WKNNF(p2)
+  knnq12<-knn_nn2_form(p1, p2, k=5)
+  knnq11<-knn_nn2_form(p1, p1, k=5)
+  knnq22<-knn_nn2_form(p2, p2, k=5)
+  knnq21<-knn_nn2_form(p2, p1, k=5)
+  
+  expect_equal(w1$query(p2, 5, 0), knnq12, tolerance=1e-6)
+  expect_equal(w1$query(p2, 5, 0), nn2(p1, p2, k=5, eps=0), tolerance=1e-6)
+  expect_equal(w1$queryWKNNF(w2$.CppObject, 5, 0), knnq12, tolerance=1e-6)
+  expect_equal(w1$queryWKNNF(w2$.CppObject, 5, 0), nn2(p1, p2, k=5, eps=0), tolerance=1e-6)
+  
+  expect_equal(w1$query(p1, 5, 0), knnq11, tolerance=1e-6)
+  expect_equal(w1$query(p1, 5, 0), nn2(p1, p1, k=5, eps=0), tolerance=1e-6)
+  expect_equal(w1$queryWKNNF(w1$.CppObject, 5, 0), knnq11, tolerance=1e-6)
+  expect_equal(w1$queryWKNNF(w1$.CppObject, 5, 0), nn2(p1, p1, k=5, eps=0), tolerance=1e-6)
+  
+  expect_equal(w2$query(p2, 5, 0), knnq22, tolerance=1e-6)
+  expect_equal(w2$query(p2, 5, 0), nn2(p2, p2, k=5, eps=0), tolerance=1e-6)
+  expect_equal(w2$queryWKNNF(w2$.CppObject, 5, 0), knnq22, tolerance=1e-6)
+  expect_equal(w2$queryWKNNF(w2$.CppObject, 5, 0), nn2(p2, p2, k=5, eps=0), tolerance=1e-6)
+
+  expect_equal(w2$query(p1, 5, 0), knnq22, tolerance=1e-6)
+  expect_equal(w2$query(p1, 5, 0), nn2(p2, p1, k=5, eps=0), tolerance=1e-6)
+  expect_equal(w2$queryWKNNF(w1$.CppObject, 5, 0), knnq21, tolerance=1e-6)
+  expect_equal(w2$queryWKNNF(w1$.CppObject, 5, 0), nn2(p2, p1, k=5, eps=0), tolerance=1e-6)
+  
+  set.seed(42)
+  d=matrix(rnorm(100*3), ncol=3)
+  q=matrix(rnorm(100*3), ncol=3)
+  wd=WKNNF(d)
+  wq=WKNNF(q)
+  expect_equal(wd$query(q, 5, 0), nn2(d, q, k=5), tolerance=1e-6)
+  expect_equal(wd$query(d, 5, 0), nn2(d, d, k=5), tolerance=1e-6)
+  expect_equal(wq$query(q, 5, 0), nn2(q, q, k=5), tolerance=1e-6)
+  expect_equal(wq$query(d, 5, 0), nn2(q, d, k=5), tolerance=1e-6)
+  
+})
