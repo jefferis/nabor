8889cf1161b5a5995c45c8240c00e875c9d26d12
Gregory Jefferis <jefferis@gmail.com>
2014-08-29 20:31:40 +0100

    Handle choice of searchtype in C++
    * this saves about 4us on my machine!
      (when 300 x 300 3D points, k=1 takes about 180us)

--- R/knn.R
+++ R/knn.R
@@ -7,31 +7,29 @@
 #'   \code{searchtype="brute"} checks all point combinations and is intended for
 #'   validation only.
 #'   
+#'   Integer values of searchtype should be the 1-indexed position in the vector
+#'   \code{c("auto", "brute", "kd_linear_heap", "kd_tree_heap")}, i.e. a value
+#'   between 1L and 4L.
+#'   
 #' @param data Mxd matrix of M target points with dimension d
 #' @param query Nxd matrix of N query points with dimension d (nb \code{data} 
 #'   and \code{query} must have same dimension)
 #' @param k an integer number of nearest neighbours to find
 #' @param eps An approximate error bound. The default of 0 implies exact 
 #'   matching.
-#' @param searchtype A character vector indicating the a search type, defaulting
-#'   to "auto". See details.
+#' @param searchtype A character vector or integer indicating the search type. 
+#'   The default value of \code{1L} is equivalent to "auto". See details.
 #' @return A list with elements \code{nn.idx} (1-indexed indices) and 
 #'   \code{nn.dists} (distances), both of which are N x k matrices
 #' @export
-knn <- function(data, query, k, eps = 0.0, 
-                searchtype=c("auto", "kd_linear_heap","kd_tree_heap","brute")) {
-  search_choices=c("auto", "kd_linear_heap","kd_tree_heap","brute")
-  searchtype=match.arg(searchtype, search_choices)
-  
-  # There are 3 types of search available via libnabo
-  # BRUTE_FORCE = 0, //!< brute force, check distance to every point in the data
-  # KDTREE_LINEAR_HEAP, //!< kd-tree with linear heap, good for small k (~up to 30)
-  # KDTREE_TREE_HEAP, //!< kd-tree with tree heap, good for large k (~from 30)
-  int_search_type = switch(searchtype,
-                           auto = ifelse(k<30, 1L, 2L),
-                           kd_linear_heap=1L,
-                           kd_tree_heap=2L,
-                           brute=0L)
-  
-  .Call('nabo_knn_generic', PACKAGE = 'nabo', int_search_type, data, query, k, eps)
+knn <- function(data, query, k, eps = 0.0, searchtype=1L) {
+  if(is.character(searchtype)) {
+    if(length(searchtype)!=1) stop("only 1 searchtype permitted!")
+    search_choices=c("auto", "brute", "kd_linear_heap", "kd_tree_heap")
+    searchtype=pmatch(searchtype, search_choices)
+    if(is.na(searchtype)) stop("Invalid search type!")
+  } else {
+    if(searchtype>4L || searchtype<1L) stop("Unknown search type!")
+  }
+  .Call('nabo_knn_generic', PACKAGE = 'nabo', searchtype, data, query, k, eps)
 }
--- src/Rnabo.cpp
+++ src/Rnabo.cpp
@@ -15,8 +15,19 @@ List knn_generic(int st, const Eigen::Map<Eigen::MatrixXd> data, const Eigen::Ma
   
   // create WKNND object but don't build tree
   WKNND tree = WKNND(data, false);
+  
+  // establish search type
+  Nabo::NNSearchD::SearchType nabo_st;
+  if(st==1L){
+    // incoming st==1 implies auto, so choose according to value of k
+    nabo_st = k < 30 ? NNSearchD::KDTREE_LINEAR_HEAP : NNSearchD::KDTREE_TREE_HEAP;
+  } else {
+    // if we receive 2L from R => BRUTE_FORCE etc
+    nabo_st = static_cast<Nabo::NNSearchD::SearchType>(st-2L);
+  }
+  
   // build tree using appropriate search type
-  tree.build_tree(static_cast<Nabo::NNSearchD::SearchType>(st));
+  tree.build_tree(nabo_st);
   
   return tree.query(query, k, eps);
 }
